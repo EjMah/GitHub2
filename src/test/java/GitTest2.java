803880f926d94c9232cb13e4429600551240d866
EjMah <eje.mahler@iths.se>
2024-10-31 11:15:14 +0100

    Initial commit

---
+++ src/test/java/GitTest2.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GitTest2 {
+
+    @Test
+    //nU SKA JAG FÖRSÖKA FEL 2
+    public void aTest(){
+        assertTrue(true);
+
+    }
+}
